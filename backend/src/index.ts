a37dbea6a80fb5a4609cdb93ae18eb493af87425
tencalink0 <bencax270@gmail.com>
2025-06-10 20:12:16 +0100

    feat: init project

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/api/hello', (req, res) => {
+    res.send('Test');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get(/.*/, (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
