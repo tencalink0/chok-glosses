33b16f985a247208b83a391cb91d0494d3574b69
tencalink0 <bencax270@gmail.com>
2025-06-22 14:00:37 +0100

    fix: (hopefully) final fix

--- backend/src/server-deno.ts
+++ backend/src/server-deno.ts
@@ -1,2 +1,2 @@
 import { serveDir } from  'jsr:@std/http/file-server'
-Deno.serve(req => serveDir(req, { fsRoot: 'dist/public' }))
+Deno.serve(req => serveDir(req, { fsRoot: 'backend/dist/public' }))
