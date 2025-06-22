3b96e4ed9ba7a94784cd794fbc6a744924fb6387
tencalink0 <bencax270@gmail.com>
2025-06-22 13:58:05 +0100

    fix: basic file serving revert

--- backend/dist/server-deno.ts
+++ backend/dist/server-deno.ts
@@ -1,23 +1,2 @@
-import { Hono } from 'https://deno.land/x/hono@v3.11.6/mod.ts';
-import { serveStatic } from 'https://deno.land/x/hono@v3.11.6/middleware.ts';
-import { dirname, fromFileUrl, join } from "https://deno.land/std@0.224.0/path/mod.ts";
-
-const __dirname = dirname(fromFileUrl(import.meta.url));
-const app = new Hono();
-const PORT = 3001;
-
-const publicDir = join(__dirname, '..', 'dist', 'public');
-
-app.get('/api/hello', (c) => c.text('Test'));
-
-app.use('/assets//*', serveStatic({ root: './dist/public/assets' }));
-app.use('/*', serveStatic({ root: './dist/public' }));
-
-app.get('/*', async (c) => {
-    const indexPath = join(publicDir, 'index.html');
-    const content = await Deno.readTextFile(indexPath);
-    return c.html(content);
-});
-
-console.log(`Server running on http://localhost:${PORT}`);
-Deno.serve({ port: PORT }, app.fetch);
+import { serveDir } from  'jsr:@std/http/file-server'
+Deno.serve(req => serveDir(req, { fsRoot: 'dist/public' }))
